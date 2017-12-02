c2f8e7827862100d5c3edb626ac3a6e266e50908
idan arazi <idan.arazi@gmail.com>
2017-12-02 15:32:11 +0200

    first commit
    first commit

---
+++ public/javascripts/pets.js
@@ -0,0 +1,100 @@
+//should be in a seperate json file
+
+var pets = [{
+    "petId":1,
+    "petName":"Bearded Dragon Lizard",
+    "petDescription":"The genus Pogona is in the subfamily Amphibolurinae of the lizard family Agamidae. Characteristics include broad, triangular heads and flattened bodies with spiny scales arranged in rows and clusters. These are found on the throat, which can be expanded when threatened, and at the back of the head. These scales are used to scare off predators, yet they are not very sharp. Bearded dragons display a hand-waving gesture to show submission, and a head-bobbing display to show dominance between dragons. They have the ability to change color during rivalry challenges between males, in response to ambient temperature changes such as turning black to absorb heat, and other stimuli. Males grow up to 60 cm (24 in) long, and females up to 51 cm (20 in). Bearded dragons also produce a mild venom originating from primitive venom glands. Although generally harmless towards humans, it is effective towards smaller-sized animals.",
+    "petImg":"./images/Lizard-small.jpg",
+    "petImgBig":"./images/bearded-dragon.jpg"
+},{
+    "petId":2,
+    "petName":"Chinchilla",
+    "petDescription":"Chinchillas are popular pets, but require much care. They should only be purchased by experienced pet owners who are aware of their needs. Chinchillas must have extensive exercise and dental care, due to their teeth continually growing all throughout their life span, and since they lack the ability to sweat, temperatures need to be carefully controlled. They should be kept in an environment of 60 to 70 °F (16 to 21 °C). Their cage should always be placed in a well-lit area, but not placed in direct sunlight or in drafts. The animals instinctively clean their fur by taking dust baths, in which they roll around in special dust made of fine pumice, a few times a week; they do not bathe in water. If they get wet they should be dried off immediately or else their fur will grow fungus and they can possibly get a skin infection. Their thick fur resists parasites, such as fleas, and reduces loose dander.",
+    "petImg":"./images/chin-small.jpg",
+    "petImgBig":"./images/chin.jpg"
+},{
+    "petId":3,
+    "petName":"Crocodile",
+    "petDescription":"Crocodiles (subfamily Crocodylinae) or true crocodiles are large aquatic reptiles that live throughout the tropics in Africa, Asia, the Americas and Australia. Crocodylinae, all of whose members are considered true crocodiles, is classified as a biological subfamily. A broader sense of the term crocodile, Crocodylidae that includes Tomistoma, is not used in this article. The term crocodile here applies to only the species within the subfamily of Crocodylinae. The term is sometimes used even more loosely to include all extant members of the order Crocodilia, which includes the alligators and caimans (family Alligatoridae), the gharial and false gharial (family Gavialidae), and all other living and fossil Crocodylomorpha. Although they appear to be similar to the untrained eye, crocodiles, alligators and the gharial belong to separate biological families. The gharial, with its narrow snout, is easier to distinguish, while morphological differences are more difficult to spot in crocodiles and alligators. The most obvious external differences are visible in the head, with crocodiles having narrower and longer heads, with a more V-shaped than a U-shaped snout compared to alligators and caimans. Another obvious trait is that the upper and lower jaws of the crocodiles are the same width, and the teeth in the lower jaw fall along the edge or outside the upper jaw when the mouth is closed; therefore, all teeth are visible, unlike an alligator, which possesses in the upper jaw small depressions into which the lower teeth fit. Also, when the crocodile's mouth is closed, the large fourth tooth in the lower jaw fits into a constriction in the upper jaw. For hard-to-distinguish specimens, the protruding tooth is the most reliable feature to define the species' family. Crocodiles have more webbing on the toes of the hind feet and can better tolerate saltwater due to specialized salt glands for filtering out salt, which are present, but non-functioning, in alligators. Another trait that separates crocodiles from other crocodilians is their much higher levels of aggression. Crocodile size, morphology, behaviour and ecology differ somewhat among species. However, they have many similarities in these areas as well. All crocodiles are semiaquatic and tend to congregate in freshwater habitats such as rivers, lakes, wetlands and sometimes in brackish water and saltwater. They are carnivorous animals, feeding mostly on vertebrates such as fish, reptiles, birds and mammals, and sometimes on invertebrates such as molluscs and crustaceans, depending on species and age. All crocodiles are tropical species that, unlike alligators, are very sensitive to cold. They separated from other crocodilians during the Eocene epoch, about 55 million years ago. Many species are at the risk of extinction, some being classified as critically endangered.",
+    "petImg":"./images/croc-small.jpg",
+    "petImgBig":"./images/croc.jpg"
+},{
+    "petId":4,
+    "petName":"Piranha",
+    "petDescription":"A piranha or piraña (/pɪˈrɑːnjə/, /pɪˈrænjə/, or /pɪˈrɑːnə/; Portuguese: [piˈɾɐ̃ɲɐ], Spanish: [piˈɾaɲa]) is a member of family Characidae in order Characiformes, freshwater fish that inhabits South American rivers, floodplains, lakes and reservoirs. Although often described as extremely predatory and mainly feeding on fish, their dietary habits vary extensively and they will also take plant material, leading to their classification as omnivorous. In Venezuela, they are called caribes. They are known for their sharp teeth and powerful jaws.",
+    "petImg":"./images/pir-small.jpg",
+    "petImgBig":"./images/pir.jpg"
+},{
+    "petId":5,
+    "petName":"Hedgehog",
+    "petDescription":"Hedgehogs are easily recognized by their spines, which are hollow hairs made stiff with keratin. Their spines are not poisonous or barbed and unlike the quills of a porcupine, do not easily detach from their bodies. However, the immature animal's spines normally fall out as they are replaced with adult spines. This is called 'quilling'. Spines can also shed when the animal is diseased or under extreme stress. A defense that all species of hedgehogs possess is the ability to roll into a tight ball, causing all of the spines to point outwards. The hedgehog's back contains two large muscles that control the position of the quills. When the creature is rolled into a ball, the quills on the back protect the tucked face, feet, and belly, which are not quilled. Since the effectiveness of this strategy depends on the number of spines, some desert hedgehogs that evolved to carry less weight are more likely to flee or even attack, ramming an intruder with the spines; rolling into a spiny ball for those species is a last resort. The various species are prey to different predators: while forest hedgehogs are prey primarily to birds (especially owls) and ferrets, smaller species like the long-eared hedgehog are prey to foxes, wolves, and mongooses. Hedgehogs are primarily nocturnal, though some species can also be active during the day. Hedgehogs sleep for a large portion of the day under bushes, grasses, rocks, or most commonly in dens dug in the ground, with varying habits among the species. All wild hedgehogs can hibernate, though not all do, depending on temperature, species, and abundance of food. Hedgehogs are fairly vocal and communicate through a combination of grunts, snuffles and/or squeals, depending on species. Self-anointing Hedgehogs occasionally perform a ritual called anointing. When the animal encounters a new scent, it will lick and bite the source, then form a scented froth in its mouth and paste it on its spines with its tongue. The purpose of this habit is unknown, but some experts believe anointing camouflages the hedgehog with the new scent of the area and provides a possible poison or source of infection to predators poked by their spines. Anointing is sometimes also called anting because of a similar behavior in birds. Like opossums, mice, and moles, hedgehogs have some natural immunity against some snake venom through the protein erinacin in the animal's muscular system, although it is only available in small amounts and a viper bite may still be fatal. In addition, hedgehogs are one of four known mammalian groups with mutations that protect against another snake venom, α-neurotoxin. Pigs, honey badgers, mongooses, and hedgehogs all have mutations in the nicotinic acetylcholine receptor that prevent the snake venom α-neurotoxin from binding, though those mutations developed separately and independently.", 
+    "petImg":"./images/hedg-small.jpg",
+    "petImgBig":"./images/hedg.jpg"
+}
+];
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
+var initPageOnLoad = function(){
+    var petId = parseInt(getUrlParameter("petId"));
+    var validPetId = 1;
+    if (Number.isInteger(petId) && petId > 0 && petId < pets.length + 1){
+        validPetId = petId
+    } 
+    for (i = 0; i < pets.length; i++) {
+        if(validPetId == i + 1){
+            $("#petMainImg").attr("src",pets[i].petImgBig);
+            $("#petMainName").html(pets[i].petName);
+            $("#petMainDesc").html(pets[i].petDescription);
+            if ($("#petMainDesc").text().length > 1860){
+                $(".read-more-container").removeClass("hide");
+            }
+            continue;
+        }
+        var newElement = "<div class='pet-list-container border flex-row' onclick='selectPet(" + pets[i].petId + ")'>";
+        newElement += "     <div class='pet-list-img border'>";
+        newElement += "         <img src='" + pets[i].petImg + "'>";
+        newElement += "     </div>";
+        newElement += "     <div class='pet-list-name bold'>";
+        newElement += "         <span>" + pets[i].petName + "</span>";
+        newElement += "     </div>";
+        newElement += "   </div>";
+        $(".pets-list-div").append(newElement);
+    } 
+}
+var toggleDiv = function() {
+    //$(".text-container").css("max-height","none");
+    var el = $(".text-container");
+    curHeight = el.height();
+    autoHeight = el.css('max-height', 'none').height();
+    if ($(".read-more-container").find("span").html() == "Read more..."){
+        el.height(curHeight).animate({height: autoHeight - 10}, 1000);
+        $(".read-more-container").find("span").html("Read less...")
+    } else{
+        el.height(autoHeight).animate({height: 358}, 1000, function() {
+            el.css("height","100%");
+            el.css("max-height","358px");
+       });
+        
+        $(".read-more-container").find("span").html("Read more...")
+    }
+    
+}
+var selectPet = function(petId){
+    window.location.href = "?petId=" + petId;
+    return false;
+}
+initPageOnLoad();
+
+
